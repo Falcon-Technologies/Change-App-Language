4ce13c273730f0e5ed399f95782f2c4b7d2cb352
Muzammil Hussain <mh66966910@gmail.com>
2022-07-19 17:20:07 +0500

    Custom Spinner Added right now

--- app/src/main/java/com/falcon/technologies/views/SelectLanguageFromSpinner.kt
+++ app/src/main/java/com/falcon/technologies/views/SelectLanguageFromSpinner.kt
@@ -1,13 +1,11 @@
 package com.falcon.technologies.views
 
 import android.os.Bundle
-import android.view.View
 import android.widget.AdapterView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
-import com.falcon.technologies.R
 import com.falcon.technologies.adapters.CustomSpinnerAdapter
 import com.falcon.technologies.databinding.ActivitySelectLanguageFromSpinnerBinding
 import com.falcon.technologies.model.CountryData
@@ -25,36 +23,36 @@ class SelectLanguageFromSpinner : AppCompatActivity() {
 
     fun setCustomAdapterSpinner() {
 
-        val country_list = arrayListOf<CountryData>()
+        val country_lists = arrayListOf<CountryData>()
 
-        country_list.add(CountryData("India"))
-        country_list.add(CountryData("United States"))
-        country_list.add(CountryData("Indonesia"))
-        country_list.add(CountryData("Pak"))
-        country_list.add(CountryData("China"))
-        country_list.add(CountryData("India"))
-        country_list.add(CountryData("United States"))
-        country_list.add(CountryData("Indonesia"))
-        country_list.add(CountryData("France"))
-        country_list.add(CountryData("China"))
-        country_list.add(CountryData("India"))
-        country_list.add(CountryData("United States"))
-        country_list.add(CountryData("Indonesia"))
-        country_list.add(CountryData("France"))
-        country_list.add(CountryData("China"))
-        country_list.add(CountryData("India"))
-        country_list.add(CountryData("United States"))
-        country_list.add(CountryData("Indonesia"))
-        country_list.add(CountryData("France"))
-        country_list.add(CountryData("China"))
+        country_lists.add(CountryData("India"))
+        country_lists.add(CountryData("United States"))
+        country_lists.add(CountryData("Indonesia"))
+        country_lists.add(CountryData("Pak"))
+        country_lists.add(CountryData("China"))
+        country_lists.add(CountryData("India"))
+        country_lists.add(CountryData("United States"))
+        country_lists.add(CountryData("Indonesia"))
+        country_lists.add(CountryData("France"))
+        country_lists.add(CountryData("China"))
+        country_lists.add(CountryData("India"))
+        country_lists.add(CountryData("United States"))
+        country_lists.add(CountryData("Indonesia"))
+        country_lists.add(CountryData("France"))
+        country_lists.add(CountryData("China"))
+        country_lists.add(CountryData("India"))
+        country_lists.add(CountryData("United States"))
+        country_lists.add(CountryData("Indonesia"))
+        country_lists.add(CountryData("France"))
+        country_lists.add(CountryData("China"))
 
-        val adapter = CustomSpinnerAdapter(this, country_list)
+        val adapter = CustomSpinnerAdapter(this, country_lists)
 
 
 
         binding.asd.setAdapter(adapter)
 
-        binding.asd.setText(country_list[3].countryName, false)
+        binding.asd.setText(country_lists[3].countryName, false)
 
         binding.asd.onItemClickListener=
             AdapterView.OnItemClickListener { p0, p1, p2, p3 ->
@@ -69,9 +67,9 @@ class SelectLanguageFromSpinner : AppCompatActivity() {
 
         // dynamically adding data after setting adapter to spinner
 
-        country_list.add(CountryData("Japan"))
-        country_list.add(CountryData("New Zealand"))
-        country_list.add(CountryData("Other"))
+        country_lists.add(CountryData("Japan"))
+        country_lists.add(CountryData("New Zealand"))
+        country_lists.add(CountryData("Other"))
 
         adapter.notifyDataSetChanged()
     }
